915ac680cfe6cc00fbd185f06a50d45bce88d122
Matthew <matthewcnitschke@gmail.com>
2017-11-30 13:24:11 -0700

    Fixed bug where script couldnt find file location

--- file-cleaner/file-cleaner.sh
+++ file-cleaner/file-cleaner.sh
@@ -2,9 +2,11 @@ DOWNLOADS_FILE_COUNT=$(ls ~/Downloads/ | wc -l)
 TRASH_FILE_COUNT=$(gvfs-ls trash:// | wc -l)
 TRASH_FILE_COUNT_THRESHOLD=30
 
-if [ $DOWNLOADS_FILE_COUNT -gt 0 ] || [ $TRASH_FILE_COUNT -gt $TRASH_FILE_COUNT_THRESHOLD ]; then
+if [ $DOWNLOADS_FILE_COUNT -gt 0 ] || [ $TRASH_FILE_COUNT -gt $TRASH_FILE_COUNT ]; then
+    SCRIPT_LOCATION=~/Scripts/file-cleaner/file-cleaner.js
+
     export DISPLAY=:0.0
-    gnome-terminal -e "node Scripts/file-cleaner/file-cleaner.js"
+    gnome-terminal -e "node $SCRIPT_LOCATION"
 fi
 
 # --window-with-profile=FileCleaner
