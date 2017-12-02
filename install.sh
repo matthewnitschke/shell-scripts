cc4e185a97eacd97add674cdae4cccdc9f461f23
Matthew <matthewcnitschke@gmail.com>
2017-12-02 16:41:27 -0700

    Fixed bugs in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # install file-cleaner dependencies
 cd file-cleaner
 yarn install
@@ -8,21 +7,20 @@ yarn install
 # add aliases to bash_aliases
 if [ -f ~/.bash_aliases ]; then
     USER_ALIASES=~/.bash_aliases
-    LOCAL_ALIASES=./.bash_aliases
+    LOCAL_ALIASES=~/Scripts/shell-scripts-aliases
     
-    while read myAliase; do 
-        if grep -q "$myAliase" $LOCAL_ALIASES; then
+    while read myAliase; do
+        if grep -q "$myAliase" $USER_ALIASES; then
             echo "Aliase: '$myAliase' already found"
         else
             echo "$myAliase # added automaticlly from ~/Scripts/install.sh" >> $USER_ALIASES
             echo "Added: '$myAliase' to ~/.bash_aliases"
         fi 
     done < $LOCAL_ALIASES
-
-    source $USER_ALIASES
 fi
 
-LOCAL_CRON=./shell-scripts-cron
+# add cron to users crontab
+LOCAL_CRON=~/Scripts/shell-scripts-cron
 while read myCron; do
     (crontab -l ; echo "$myCron") | sort - | uniq - | crontab -
 done < $LOCAL_CRON
