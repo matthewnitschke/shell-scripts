7f98a3410d12bb48ecdeef49dbd922083a72ff37
Matthew <matthewcnitschke@gmail.com>
2017-12-09 15:24:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,3 +25,11 @@ while read myCron; do
     (crontab -l ; echo "$myCron") | sort - | uniq - | crontab -
 done < $LOCAL_CRON
 echo "contab updated"
+
+
+# add sublime text 3 cli command (subl)
+if [ -f /usr/local/bin/subl ]; then
+    echo "sublime text 3 cli command already exists"
+else
+    sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/subl
+fi
